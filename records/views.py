1a5646880999936375fc297d41b63553e84afeba
Shikhar Bakhda <ssb2189@columbia.edu>
2017-02-24 00:00:44 -0500

    removed login system

--- records/views.py
+++ records/views.py
@@ -69,82 +69,82 @@ class PiDelete(generic.DeleteView) :
     model = Pi
     success_url = reverse_lazy('records:index')
 
-class UserFormView(View) :
-    form_class = UserForm
-    template_name = 'records/registration_form.html'
+# class UserFormView(View) :
+#     form_class = UserForm
+#     template_name = 'records/registration_form.html'
+#
+#     def get(self, request):
+#         form = self.form_class(None)
+#         return render(request, self.template_name, {'form' : form})
+#
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#
+#         if form.is_valid() :
+#             user = form.save(commit=False)
+#
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user.set_password(password)
+#             user.save()
+#
+#             user = authenticate(username=username, password=password)
+#
+#             if user is not None :
+#                 if user.is_active :
+#                     login(request, user)
+#                     return redirect('records:index')
+#
+#         return render(request, self.template_name, {'form': form})
 
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form' : form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-
-        if form.is_valid() :
-            user = form.save(commit=False)
-
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-
-            user = authenticate(username=username, password=password)
-
-            if user is not None :
-                if user.is_active :
-                    login(request, user)
-                    return redirect('records:index')
-
-        return render(request, self.template_name, {'form': form})
 
 
+# def logout_user(request) :
+#     logout(request)
+#     form = UserForm(request.POST or None)
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, 'records/login.html', context)
 
-def logout_user(request) :
-    logout(request)
-    form = UserForm(request.POST or None)
-    context = {
-        "form": form,
-    }
-    return render(request, 'records/login.html', context)
+# def home(request) :
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 all_pi = Pi.objects.filter(user=request.user)
+#                 return render(request, 'records/home.html', {'all_pi': all_pi})
+#             else:
+#                 return render(request, 'records/home.html', {'error_message': 'Your account has been disabled'})
+#         else:
+#             return render(request, 'records/home.html', {'error_message': 'Invalid login'})
+#     return render(request, 'records/home.html')
 
-def home(request) :
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                all_pi = Pi.objects.filter(user=request.user)
-                return render(request, 'records/home.html', {'all_pi': all_pi})
-            else:
-                return render(request, 'records/home.html', {'error_message': 'Your account has been disabled'})
-        else:
-            return render(request, 'records/home.html', {'error_message': 'Invalid login'})
-    return render(request, 'records/home.html')
+# def login_user(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 all_pi = Pi.objects.all()
+#                 request.session['username'] = username
+#                 return render(request, 'records/index.html', {'all_pi': all_pi})
+#             else:
+#                 return render(request, 'records/login.html', {'error_message': 'Your account has been disabled'})
+#         else:
+#             return render(request, 'records/login.html', {'error_message': 'Invalid login'})
+#     return render(request, 'records/login.html')
 
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                all_pi = Pi.objects.all()
-                request.session['username'] = username
-                return render(request, 'records/index.html', {'all_pi': all_pi})
-            else:
-                return render(request, 'records/login.html', {'error_message': 'Your account has been disabled'})
-        else:
-            return render(request, 'records/login.html', {'error_message': 'Invalid login'})
-    return render(request, 'records/login.html')
-
-def formView(request):
-   if request.session.has_key('username'):
-       pass
-      # username = request.session['username']
-      # return render(request, 'index.html', {"username" : username})
-   else:
-      return render(request, 'login.html', {})
+# def formView(request):
+#    if request.session.has_key('username'):
+#        pass
+#       # username = request.session['username']
+#       # return render(request, 'index.html', {"username" : username})
+#    else:
+#       return render(request, 'login.html', {})
 
