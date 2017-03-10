4e894aa3b86bae1d2bd2f26104e38c8a49768c0e
Shikhar Bakhda <ssb2189@columbia.edu>
2017-03-10 00:05:39 -0500

    added datetime field

--- records/models.py
+++ records/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from datetime import datetime, timedelta
 
 class Pi(models.Model) :
     address = models.CharField(max_length=100)
@@ -14,7 +15,7 @@ class Pi(models.Model) :
 
 class Data(models.Model) :
     pi = models.ForeignKey(Pi, on_delete= models.CASCADE)
-    date_time = models.IntegerField()
+    date_time = models.DateTimeField()
     raw_vol = models.DecimalField(max_digits=5, decimal_places=1)
     las_vol = models.DecimalField(max_digits=5, decimal_places=1)
     new_vol = models.DecimalField(max_digits=5, decimal_places=1)
@@ -22,5 +23,6 @@ class Data(models.Model) :
     status = models.CharField(max_length=10)
 
     def __str__(self):
-        return str(self.date_time) + ' - ' + str(self.pi.pk)
+        return str(self.date_time.strftime('%x/%X')) + ' - ' + str(self.pi.pk)
+
 
